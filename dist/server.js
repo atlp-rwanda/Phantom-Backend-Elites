a184f4aff5ca8c9a573ebabb52328576f6c2a7fa
Emmanuel ASIFIWE <emmanuel2koestner@gmail.com>
2022-02-17 17:33:29 +0200

    allowing app to run on port

--- dist/server.js
+++ dist/server.js
@@ -2,9 +2,18 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express.default)();
+
+_dotenv.default.config();
+
+const port = process.env.PORT | 3000;
 app.get("/", (req, res, next) => {
   res.send("<h1>Welcome to phantom app</h1>");
+});
+app.listen(port, () => {
+  console.log(`App is running on ${port}`);
 });
