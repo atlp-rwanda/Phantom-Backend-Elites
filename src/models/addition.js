dd16e5cae7bebdf392e19545a5c5494f49d2a6a2
Emmanuel ASIFIWE <emmanuel2koestner@gmail.com>
2022-02-19 08:55:57 +0200

    addition of extra structure with badge

--- src/models/addition.js
+++ src/models/addition.js
@@ -1,10 +1,16 @@
-const add = (x, y) => {
+/* eslint-env browser */
+
+const add = function (x, y) {
   console.log(`value of x:${x} and value of y:${y}`);
-  if (x < 0) {
-    console.log("x should be greater than 0");
-  } else {
-    console.log(x + y);
-  }
+  // eslint-disable-next-line no-undef
+  return new Promise((resolve, reject) => {
+    if (x < 0) {
+      reject("x should be greater than 0");
+    } else {
+      resolve(x + y);
+    }
+  });
 };
 
-export default add;
+// eslint-disable-next-line no-undef
+exports.add = add;
