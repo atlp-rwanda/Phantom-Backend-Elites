893a9d4f1f3456e67be1c81117e4acf65a674dba
niyomozey <moiseniyonkuru1@gmail.com>
2022-02-20 13:10:31 +0200

    Changes added to support multiple language setup

--- src/app.js
+++ src/app.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import express from 'express';
-import i18next from './config/i18nConf';
-import middleware from "i18next-express-middleware";
-
-var app = express()
-const port = process.env.PORT || 1110;
-
-app.use(middleware.handle(i18next,{
-    ignoreRoutes: ["/foo"], //ignore route from being internationalize ex:/foo
-    removeLngFromUrl: false
-  }))
-
-app.get('/home',(req, res, next)=>{
-    res.send({message: req.t('hello_world')})
-})
-
-=======
 // import express from 'express'
 // import i18n from 'i18n'
 const express = require('express')
@@ -53,7 +35,6 @@ const port = process.env.PORT || 1111;
 app.use('/home',(req, res, next)=>{
     res.send('hello world')
 })
->>>>>>> 1253fac (Change on i18m configuration)
 app.listen(port, ()=>{
     console.log(`Connected : ${port} port`)
 })
