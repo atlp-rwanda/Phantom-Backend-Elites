d97fa7ac8b7902764834cd9e0feb409834b5928e
Iragena Fides Noella <fniragena@gmail.com>
2022-03-01 22:12:56 +0200

    Change on i18m configuration

--- src/app.js
+++ src/app.js
@@ -1,19 +1,40 @@
-import express from 'express';
-import i18next from './config/i18nConf';
-import middleware from "i18next-express-middleware";
-
+// import express from 'express'
+// import i18n from 'i18n'
+const express = require('express')
+// const i18n = require('i18n-express')
+const i18 = require('i18n')
+const path = require('path')
+var i18next = require('i18next')
+var middleware = require("i18next-express-middleware");
+const i18n = require('./shared/i18n');
 var app = express()
-const port = process.env.PORT || 1110;
 
-app.use(middleware.handle(i18next,{
-    ignoreRoutes: ["/foo"], //ignore route from being internationalize ex:/foo
-    removeLngFromUrl: false
-  }))
+// i18.configure({
+//     defaultLocale: 'en',
+//     // directory: 'i18n/locales',
+//     api: {
+//       __: 'translate',
+//       __n: 'translateN',
+//     },
+// });
+app.use(i18n);
 
-app.get('/home',(req, res, next)=>{
-    res.send({message: req.t('hello_world')})
+// i18next.use(middleware.LanguageDetector).init({
+//     preload: ["en", "de", "it"]
+//   });
+// app.use(
+//     middleware.handle(i18next, {
+//       ignoreRoutes: ["/foo"], // or function(req, res, options, i18next) { /* return true to ignore */ }
+//       removeLngFromUrl: false
+//     })
+//   );
+
+
+const port = process.env.PORT || 1111;
+
+app.use('/home',(req, res, next)=>{
+    res.send('hello world')
 })
-
 app.listen(port, ()=>{
     console.log(`Connected : ${port} port`)
 })
