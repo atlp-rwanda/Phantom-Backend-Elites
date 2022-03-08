8480ed1b8fd04a94519c62d715f3175c22c66891
niyomozey <moiseniyonkuru1@gmail.com>
2022-03-08 02:09:01 +0200

    validation updated

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,87 +1,100 @@
+import Joi from 'joi'
+import Users from '../../sequelize/models/user'
+import { development } from "../../sequelize/config/config.js";
+import { Sequelize } from "sequelize";
+import Roles from '../../sequelize/models/role'
+let sequelize = new Sequelize(development);
+let User = Users(sequelize, Sequelize);
+let Role = Roles(sequelize, Sequelize)
 
 class Validate{
     loginFields(req, res, next){
-        try{
-            const checkNull = req.body.email == undefined || req.body.password == undefined ? true : false;
-
-            if(checkNull){
-                throw new Error('There is missing field')
-            }   
-            if(!req.body.email){
-                throw new Error('Email field can\'t be empty')
-            }
-            if(!req.body.password){
-                throw new Error('Password field can\'t be empty')
-            }
-            const isEmailValid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email);
-            if(!isEmailValid){
-                throw new Error('You have entered an invalid email address!')
-            }
-            next()
-        }catch(error){
-            res.status(400).send({message: error.message})
+        const schema = Joi.object({
+            email: Joi.string().required()
+            .email({ minDomainSegments: 2}),
+            password: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body,{ abortEarly: false })
+        if(error){
+            const { details } = error;
+            const errors = {};
+            for (let item of details) errors[item.path[0]] = item.message;
+            return res.status(400).send(errors)
         }
-        
+        next()        
     }
     
     
-    userFields(req, res, next){
-        const errors = {
-            firstName: "",
-            lastName: "",
-            email: "",
-            roleId: "",
-            dateofbirth:"",
-            gender: "",
-            address:"",
+    async userFields(req, res, next){
+        
+        const schema = Joi.object({
+            email: Joi.string().required()
+            .email({ minDomainSegments: 2}),
+            firstName: Joi.string().required(),
+            lastName: Joi.string().required(),
+            roleId: Joi.number().min(2).required().messages({
+                "number.min": "Choose another role"
+            }),
+            dateofbirth: Joi.string().required(),
+            gender: Joi.string().required(),
+            address: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body,{ abortEarly: false })
+        if(error){
+            const { details } = error;
+            const errors = {};
+            for (let item of details) errors[item.path[0]] = item.message;
+            return res.status(400).send(errors)
         }
-        try{
-            const checkNull = req.body.email == undefined;
-
-            if(checkNull){
-                errors.email = "There is missing field";
-            }   
-            const isEmailValid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email);
-            if(!isEmailValid){
-                errors.email ="You have entered an invalid email address!";
-            }
-            if(req.body.firstName == undefined || req.body.firstName == ''){
-                errors.firstName = "Missing firstname"
-            }
-            if(req.body.lastName == undefined || req.body.lastName == ''){
-                errors.lastName = "Missing lastName"
-            }
-            if(req.body.roleId == undefined || req.body.roleId == ''){
-                errors.roleId = "Missing roleId"
-            }
-            if(req.body.dateofbirth == undefined || req.body.dateofbirth == ''){
-                errors.dateofbirth = "Missing dateofbirth"
-            }
-            if(req.body.gender == undefined || req.body.gender == ''){
-                errors.gender = "Missing gender"
-            }
-            if(req.body.address == undefined || req.body.address == ''){
-                errors.address = "Missing address"
-            }
+        const user = await User.findOne({ where: { email: req.body.email } });
+        if (user) return res.status(400).json({ message: "User email already exist" });
+        const role = await Role.findOne({ where: { id: req.body.roleId } });
+        if (!role) return res.status(400).json({ message: "Role doesn't exist" });
+        next()
+        
+    }
+    async createRole(req, res, next){
+        const schema = Joi.object({
+            id: Joi.number(),
+            name: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body,{ abortEarly: false })
+        if(error){
+            const { details } = error;
+            const errors = {};
+            for (let item of details) errors[item.path[0]] = item.message;
+            return res.status(400).send(errors)
+        }
+        const role = await Role.findOne({ where: { name: req.body.name.toLowerCase() } });
+        if (role) return res.status(400).json({ message: "Role already exist" });
+        const roles = ['driver','dperator','admin']
+        if(!roles.includes(req.body.name)){
+            return res.status(400).json({ message: "Unknow role can't be created" });
+        }
+        next()
+    }
+    async updateRole(req, res, next){
+        const schema = Joi.object({
+            name: Joi.string().required()
+        })
+        const {error, value} = schema.validate(req.body,{ abortEarly: false })
+        if(error){
+            const { details } = error;
+            const errors = {};
+            for (let item of details) errors[item.path[0]] = item.message;
+            return res.status(400).send(errors)
+        }
+        try {
+            const role = await Role.findOne({ where: { id: req.params.id } });
+            if (!role) return res.status(400).json({ message: "Role you want to update doesn't exist" });
             
-            var counter = 0;
-            for(var key in errors){
-                if(errors[key]!=""){
-                    ++counter;
-                }else{
-                    delete errors[key];
-                }
-            }
-              
-            if(counter!=0){
-                throw new Error()
-            }
-            next()
-        }catch(error){
-            res.status(400).send({errors})
+            next() 
+        } catch (error) {
+            return res.status(400).json({ message: "Role you want to update doesn't exist" });
         }
         
     }
+
 }
 
 export default new Validate()
