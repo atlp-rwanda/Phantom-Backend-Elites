97884da489c8879366376b8be524d8314887d4fa
nirifabien1234 <nirifabien@gmail.com>
2022-03-31 20:50:59 +0200

    Added container for github actions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -58,7 +58,7 @@ class UserController {
         exclude: ['password']
     }})
       .then(data => {
-        
+        console.log(data)
         res.status(200).json({message:'List of all available users.',data});
 
       })
