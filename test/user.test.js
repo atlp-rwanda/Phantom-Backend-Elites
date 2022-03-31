75bfeda6dded9d4d5901a98fd134918720f9c2b6
nirifabien1234 <nirifabien@gmail.com>
2022-03-31 19:34:56 +0200

    Added container for github actions

--- test/user.test.js
+++ test/user.test.js
@@ -8,30 +8,15 @@ import { adminMock,
     userMock,userToDelete} from './mock/userMocks';
 
 
-  
-
-    // let token
-    // beforeEach( (done)=> {
-    //     setup.chai
-    //      .request(setup.app)
-    //      .post('/api/v1/auth/login')
-    //      .send(adminMock)
-    //      .end((err, res) =>{
-    //       if (err) done(err)
-    //       done()
-    //       return token = `Bearer ${res.body.token}`
-          
-    //     })
-    // })
     const login = async (user) => {
       const tokenData = await chai.request(app).post('/api/v1/auth/login').send(user);
       return `Bearer ${tokenData.body.data.token}`;
     };
-
+    
 describe('User related Tests', async () => {
-
+  const token = login(adminMock)
     it ('Admin should not create a user without firstName', (done) => {
-      const token = login(adminMock)
+      
       setup.chai
           .request(setup.app)
           .post('/api/v1/users')
@@ -46,166 +31,152 @@ describe('User related Tests', async () => {
           })
       });
     
-//     it ('Admin should not create a user without lastName', async () => {
-//       const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-//           const res =  await setup.chai
-//             .request(setup.app)
-//             .post('/api/v1/users')
-//             .send(userWithoutLastName)
-//             .set('authorization', token);
-//           setup.expect(res.status).to.be.equal(400);
-//           setup.expect(res.body).to.have.property('lastName', '"lastName" is required');
-//         });
-//     it ('Admin should not create a user without email', async () => {
-//       const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-  
-//               const res =  await setup.chai
-//                 .request(setup.app)
-//                 .post('/api/v1/users')
-//                 .send(userWithoutEmail)
-//                 .set('authorization', token);
-//               setup.expect(res.status).to.be.equal(400);
-//               setup.expect(res.body).to.have.property('email', '"email" is required');
-//             });
-//     it ('Admin should not create a user without gender', async () => {
-//       const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-//             const res =  await setup.chai
-//             .request(setup.app)
-//             .post('/api/v1/users')
-//             .send(userWithoutGender)
-//             .set('authorization', token);
-//             setup.expect(res.status).to.be.equal(400);
-//             setup.expect(res.body).to.have.property('gender', '"gender" is required');
-//         });
-//     it ('Admin should not create a user without assigning him a role', async () => {
-//       const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-//             const res =  await setup.chai
-//             .request(setup.app)
-//             .post('/api/v1/users')
-//             .send(userWithoutRole)
-//             .set('authorization', token);
-//             setup.expect(res.status).to.be.equal(400);
-//             setup.expect(res.body).to.have.property('roleId', '"roleId" is required');
-//         });
-
-
-//       it ('Should Delete a user as Admin', async () => {
-//         const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-//         const id =3
-    
-//         const res1 =  await setup.chai
-//           .request(setup.app)
-//           .delete(`/api/v1/users/${id}`)
-//           .set('authorization', token); 
-//         setup.expect(res1.status).to.be.equal(200);
-//         setup.expect(res1.body).to.have.property('message', 'User was deleted successfully!');
+    it ('Admin should not create a user without lastName', async () => {
       
-//       });
+          const res =  await setup.chai
+            .request(setup.app)
+            .post('/api/v1/users')
+            .send(userWithoutLastName)
+            .set('authorization', token);
+          setup.expect(res.status).to.be.equal(400);
+          setup.expect(res.body).to.have.property('lastName', '"lastName" is required');
+        });
+    it ('Admin should not create a user without email', async () => {
+      
+  
+              const res =  await setup.chai
+                .request(setup.app)
+                .post('/api/v1/users')
+                .send(userWithoutEmail)
+                .set('authorization', token);
+              setup.expect(res.status).to.be.equal(400);
+              setup.expect(res.body).to.have.property('email', '"email" is required');
+            });
+    it ('Admin should not create a user without gender', async () => {
+      
+            const res =  await setup.chai
+            .request(setup.app)
+            .post('/api/v1/users')
+            .send(userWithoutGender)
+            .set('authorization', token);
+            setup.expect(res.status).to.be.equal(400);
+            setup.expect(res.body).to.have.property('gender', '"gender" is required');
+        });
+    it ('Admin should not create a user without assigning him a role', async () => {
+      
+            const res =  await setup.chai
+            .request(setup.app)
+            .post('/api/v1/users')
+            .send(userWithoutRole)
+            .set('authorization', token);
+            setup.expect(res.status).to.be.equal(400);
+            setup.expect(res.body).to.have.property('roleId', '"roleId" is required');
+        });
 
-//       it ('Should Get a specific user as Admin', async () => {
-//         const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-//         const id =2
 
-//         const res1 = await setup.chai
-//         .request(setup.app)
-//         .get(`/api/v1/users/${id}`)
-//         .set('authorization', token);
-//         setup.expect(res1.status).to.be.equal(200);
-//         setup.expect(res1.body).to.have.key('data');
-//       });
+      it ('Should Delete a user as Admin', async () => {
+        
+        const id =3
+    
+        const res1 =  await setup.chai
+          .request(setup.app)
+          .delete(`/api/v1/users/${id}`)
+          .set('authorization', token); 
+        setup.expect(res1.status).to.be.equal(200);
+        setup.expect(res1.body).to.have.property('message', 'User was deleted successfully!');
+      
+      });
+
+      it ('Should Get a specific user as Admin', async () => {
+        
+        const id =2
+
+        const res1 = await setup.chai
+        .request(setup.app)
+        .get(`/api/v1/users/${id}`)
+        .set('authorization', token);
+        setup.expect(res1.status).to.be.equal(200);
+        setup.expect(res1.body).to.have.key('data');
+      });
 
     
-//       it ('Should Get all users as Admin', async () => {
-//         const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-//         const res = await setup.chai
-//         .request(setup.app)
-//         .get('/api/v1/users')
-//         .set('authorization', token);
-//         setup.expect(res.status).to.be.equal(200);
-//         setup.expect(res.body).to.have.keys('message', 'data');
+      it ('Should Get all users as Admin', async () => {
+        
+        const res = await setup.chai
+        .request(setup.app)
+        .get('/api/v1/users')
+        .set('authorization', token);
+        setup.expect(res.status).to.be.equal(200);
+        setup.expect(res.body).to.have.keys('message', 'data');
        
-//       });
+      });
 
-//       it ('Should Register new user as Admin', async () => {
-//         const userData = await setup.chai.request(setup.app).post('/api/v1/auth/login').send(adminMock);
-//       token = `Bearer ${userData.body.token}`;
-
-
-//             const res1= await setup.chai
-//             .request(setup.app)
-//             .post('/api/v1/users')
-//             .send(userMock)
-//             .set('authorization', token)
-//             setup.expect(res1.status).to.be.equal(201);
-//             setup.expect(res1.body).to.have.keys('message','data');
+      it ('Should Register new user as Admin', async () => {
+            const res1= await setup.chai
+            .request(setup.app)
+            .post('/api/v1/users')
+            .send(userMock)
+            .set('authorization', token)
+            setup.expect(res1.status).to.be.equal(201);
+            setup.expect(res1.body).to.have.keys('message','data');
             
-//         });
+        });
 
         
-//   it (`It should return a sucessfully login message`, (done) => {
-//     setup.chai
-//       .request(setup.app)
-//       .post("/api/v1/auth/login")
-//       .send(adminMock)
-//       .end((err, response) => {
-//         if (err) done(err)
-//         response.should.have.status(200);
-//         response.body.should.have.property("message");
-//         setup
-//           .expect(response.body.message)
-//           .to.equals('A token for your session has been saved!')
-//         done()
-//       });   
-//   });
- it('It should pass', (done) => {
-     setup.expect('true').to.equals('true')
-     done()
-   })
+        
+        
+        
+        it (`It should return unsucessfully login message for incorrect password`, () => {
+          const user = {
+            email: "admin@admin.com",
+            password: "admin123456"
+          }
+          setup.chai
+      .request(setup.app)
+      .post("/api/v1/auth/login")
+      .send(user)
+      .end((err, response) => {
+        response.should.have.status(404);
+        response.body.should.have.property("message");
+        setup.expect(response.body.message)
+          .to.equals('Incorrect email or password');
+      });
+  });
+  it (`It should return unsucessfully login`, async () => {
+    const user = {
+      email: "admin1@gmail.com",
+      password: "admin"
+    }
+    const res = await setup.chai
+      .request(setup.app)
+      .post("/api/v1/auth/login")
+      .send(user)
+      setup.expect(res.status).to.be.equal(400)
+      setup.expect(res.body).to.have.property('message', 'Wrong email detected!')
 
-
-//   it (`It should return unsucessfully login message for incorrect password`, () => {
-//     const user = {
-//       email: "admin@admin.com",
-//       password: "admin123456"
-//     }
-//     setup.chai
-//       .request(setup.app)
-//       .post("/api/v1/auth/login")
-//       .send(user)
-//       .end((err, response) => {
-//         response.should.have.status(200);
-//         response.body.should.have.property("message");
-//         setup.expect(response.body.message)
-//           .to.equals('Incorrect email or password');
-//       });
-//   });
-//   it (`It should return unsucessfully login`, async () => {
-//     const user = {
-//       email: "admin1@gmail.com",
-//       password: "admin"
-//     }
-//     const res = await setup.chai
-//       .request(setup.app)
-//       .post("/api/v1/auth/login")
-//       .send(user)
-//       setup.expect(res.status).to.be.equal(400)
-//       setup.expect(res.body).to.have.property('message', 'Wrong email detected!')
-
-//   });
-//   it ('Should log user out successfully', async () => {
+  });
+  it ('Should log user out successfully', async () => {
     
-//     const res = await setup.chai
-//     .request(setup.app)
-//     .post('/api/v1/auth/logout')
-//     .set('authorization', token)
-//     setup.expect(res.status).to.be.equal(200)
-//     setup.expect(res.body).to.have.property('message', 'Logged out successfully!')
-//   });
+    const res = await setup.chai
+    .request(setup.app)
+    .post('/api/v1/auth/logout')
+    .set('authorization', token)
+    setup.expect(res.status).to.be.equal(200)
+    setup.expect(res.body).to.have.property('message', 'Logged out successfully!')
+  });
+  it (`It should return a sucessfully login message`, (done) => {
+    setup.chai
+      .request(setup.app)
+      .post("/api/v1/auth/login")
+      .send(adminMock)
+      .end((err, response) => {
+        if (err) done(err)
+        response.should.have.status(200);
+        response.body.should.have.property("message");
+        setup
+          .expect(response.body.message)
+          .to.equals('A token for your session has been saved!')
+        done()
+      });   
+  });
     })
