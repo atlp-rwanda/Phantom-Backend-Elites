7e06608d9b39e9be9d25ac34ae0ce08a88c6fda7
John Niyontwali <nijohn0006@gmail.com>
2022-05-10 13:14:23 +0200

    reset password modified

--- src/routes/permissionRoutes.js
+++ src/routes/permissionRoutes.js
@@ -3,10 +3,10 @@ import isAdmin from "../middleware/isAdmin.js";
 import validate from '../middleware/validator'
 const router = express.Router();
 import permissionController from '../controllers/PermissionController.js'
-router.post("/", isAdmin, validate.createPermission, new permissionController().createPermission)
-router.get("/", isAdmin, new permissionController().findAllPermissions)
-router.put("/:id", isAdmin,validate.updatePermission, new permissionController().updatePermission)
-router.delete("/:id", isAdmin, new permissionController().deletePermission)
-router.get("/:id", isAdmin, new permissionController().findOnePermission)
+router.post("/", validate.createPermission, new permissionController().createPermission)
+router.get("/", new permissionController().findAllPermissions)
+router.put("/:id",validate.updatePermission, new permissionController().updatePermission)
+router.delete("/:id", new permissionController().deletePermission)
+router.get("/:id", new permissionController().findOnePermission)
 
 export default router
